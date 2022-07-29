50aceb5e4bcede74ecc0fe08e0d0e4aef6c86fa7
g.miki <gabriel.miki@wr.sc.usp.br>
2022-07-29 16:17:32 -0300

    Added weeks 2, 3 and 5 tasks and the firt html/css project.

---
+++ Week5_PHP/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <title> Gabriel Miki PHP </title>
+    </header>
+
+    <body> 
+        <h1> Gabriel Miki PHP Task </h1>
+
+        <p> The SHA256 hash of "Gabriel Miki" is <?php print hash("sha256", "Gabriel Miki"); ?> </p>
+
+        <pre style = "display: block; white-space: pre; margin-right: 0px;">
+ASCII Art:
+
+    ************* 
+    **
+    **     
+    **     ******
+    **         **
+    **         ** 
+    *************
+        </pre>  
+
+        <a href = "check.php">
+            Error Setting Way!
+        </a>
+
+        <br>
+
+        <a href = "fail.php">
+            Causing a Traceback Way!
+        </a>
+    </body>
+</html>
